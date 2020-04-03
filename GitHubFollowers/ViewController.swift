223d7393d618d3c7ce1f445f6086d80df81c83c5
Collin Browse <cbrowse13@gmail.com>
2020-04-03 16:02:34 -0600

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Collin Browse on 4/3/20.
+//  Copyright © 2020 Collin Browse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
